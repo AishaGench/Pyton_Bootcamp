3fed2e1d2aeb0c4a5b452ece19e372faa98c9c00
Aisha Gench <aisha.gench@gmail.com>
2023-09-18 21:36:21 -0500

    Created employee_info.py

---
+++ practice_tasks1/employee_info.py
@@ -0,0 +1,24 @@
+"""
+1. Create a python file named employee_info. declare the following variables:
+                1. name
+                2. age
+                3. gender
+                4. company
+                5. jobTitle
+                6. yearsOfExperience
+                7. salary
+                8. is_fullTime
+                9. is_married
+                10. employee_id
+"""
+# Employee information variables
+name = "Aisha Gench"
+age = 30
+gender = "Female"
+company = "Cydeo"
+jobTitle = "Software Engineer"
+yearsOfExperience = 5
+salary = 75000.00
+is_fullTime = True
+is_married = False
+employee_id = "EMP12345"
