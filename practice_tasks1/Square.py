22a6f26f158185443d515e6cf26856d7ed12837b
Aisha Gench <aisha.gench@gmail.com>
2023-09-18 21:42:46 -0500

    Created Square.py

---
+++ practice_tasks1/Square.py
@@ -0,0 +1,20 @@
+"""
+4. Create a python file named Square, write a program that can calculate the area & perimeter of any given square
+
+        Hint: side need to be given to calculate the area and perimeter of square
+            Ex:
+                Given Data:
+                    side = 5
+
+                output:
+                    Area of the square is equal to 25
+                    Perimeter of square is equal to 20
+"""
+side = 5
+
+# Calculate the area and perimeter of the square
+area = side * side
+perimeter = 4 * side
+
+print("Area of the square is equal to", area)
+print("Perimeter of the square is equal to", perimeter)
