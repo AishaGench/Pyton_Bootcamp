c1fef895512b06adb9a620f7d5d48be42e240d24
Aisha Gench <aisha.gench@gmail.com>
2023-09-18 21:44:14 -0500

    Added code in Rectangle.py

---
+++ practice_tasks1/Rectangle.py
@@ -0,0 +1,22 @@
+"""
+5. Create a python file named Rectangle, write a program that can calculate the area & perimeter of any given Rectangle
+
+        Hint: width and length need to be given to calculate the area and perimeter of rectangle
+            Ex:
+                Given Data:
+                    length = 5
+                    width = 2
+
+                output:
+                    Area of the rectangle is equal to 10
+                    Perimeter of rectangle is equal to 14
+"""
+length = 5
+width = 2
+
+# Calculate the area and perimeter of the square
+area = length * width
+perimeter = (length + width)*2
+
+print("Area of the rectangle is equal to", area)
+print("Perimeter of the rectangle is equal to", perimeter)
